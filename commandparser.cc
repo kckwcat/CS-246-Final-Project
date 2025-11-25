6d87c43e30f4e6141d0e19cbe221105f811370a2
Chuckyecheesy <80786646+Chuckyecheesy@users.noreply.github.com>
2025-11-25 13:55:06 -0500

    Add files via upload

---
+++ commandparser.cc
@@ -0,0 +1,22 @@
+import <string>;
+import <vector>;
+import <memory>;
+import <algorithm>;
+import Game;
+class Game{
+    std::unique_ptr<Game> game;
+    public:
+    void abilitySequence(){
+	game->getCurrentPlayer().playAbility();
+    }
+    void run(){
+        game->start();
+    }
+    void execute(string cmd){
+        game->getCurrentPlayer();
+        game->nextTurn();
+    }
+    void quit(){
+        game->endGame();
+    }
+}
