08db02cfe5d472ff57a462b3ab81a2fbc5334d63
Chuckyecheesy <80786646+Chuckyecheesy@users.noreply.github.com>
2025-11-27 18:18:24 -0500

    Add BoostAbility module with basic structure

---
+++ boostability.cc
@@ -0,0 +1,12 @@
+export module boostAbility;
+import <iostream>;
+import <memory>;
+import <vector>;
+import <string>;
+import Ability;
+import player;
+export struct BoostAbility : public Ability {
+    BoostAbility(Player* p=nullptr);
+    string name() const override;
+    bool use(Game &game, const vector<string>& args) override;
+};
