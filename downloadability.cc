8044db8a64988694cb79efaf2f18789f57905f1a
Chuckyecheesy <80786646+Chuckyecheesy@users.noreply.github.com>
2025-11-27 18:35:09 -0500

    Add DownloadAbility structure with basic methods

---
+++ downloadability.cc
@@ -0,0 +1,13 @@
+import <iostream>;
+import <memory>;
+import <string>;
+import <vector>;
+import Ability;
+import Game;
+import Link;
+import player;
+export struct DownloadAbility : public Ability {
+    DownloadAbility(Player* p=nullptr);
+    string name() const override;
+    bool use(Game &game, const vector<string>& args) override;
+};
