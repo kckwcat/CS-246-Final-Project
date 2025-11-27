a13ac0a1b8d1fd6c6a787f8517c7eb95def73264
Chuckyecheesy <80786646+Chuckyecheesy@users.noreply.github.com>
2025-11-27 18:44:11 -0500

    Update scanability.cc

--- scanability.cc
+++ scanability.cc
@@ -8,7 +8,7 @@ import Game;
 import Link;
 import player;
 
-struct ScanAbility : public Ability {
+export struct ScanAbility : public Ability {
     ScanAbility(Player* p=nullptr);
     string name() const override;
     bool use(Game &game, const vector<string>& args) override;
