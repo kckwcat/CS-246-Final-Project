682f24ac6410e44407a26d9085dd0fbbe224ee0c
bbabirad <bbabirad@uwaterloo.ca>
2025-11-27 21:40:12 -0500

    Add Board module with Pos struct and Board class

---
+++ Board_2.cc
@@ -0,0 +1,44 @@
+export module Board;
+import <iostream>;
+import <vector>;
+import <memory>;
+import <string>;
+import Link;
+using namespace std;
+
+
+export struct Pos {
+        int r;
+        int c;
+};
+
+export class Board {
+        //Max_row and Column
+        int R;
+        int C;
+        vector<vector<shared_ptr<Link>>> grid;
+        vector<vector<int>> firewalls;
+
+Board();
+
+bool inBounds(const Pos &p) const;
+
+shared_ptr<Link> get(const Pos &p) const;
+
+void place(const std::shared_ptr<Link>& L, const Pos &p);
+
+void clearPos(const Pos &p);
+
+void setFirewall(const Pos &p, char ch);
+
+char firewallAt(const Pos &p) const;
+
+void removeFirewall(const Pos &p);
+
+bool isServer(const Pos &p, int playerID) const;
+
+bool findLinkPos(const std::shared_ptr<Link>& L, Pos &out) const;
+
+bool findLinkPosRaw(const Link* raw, Pos &out) const;
+
+};
